58ca0bbc1c60d1d993768be86553f5434f4857f5
zsc <2736629976@qq.com>
2019-05-24 20:59:56 +0800

    同时支持http和https

--- src/main/java/cn/fengyunxiao/nest/NestApplication.java
+++ src/main/java/cn/fengyunxiao/nest/NestApplication.java
@@ -1,15 +1,14 @@
 package cn.fengyunxiao.nest;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -20,30 +19,20 @@ public class NestApplication {
         SpringApplication.run(NestApplication.class);
     }
 
+    // application.yml 配置了 ssl 后，默认使用 https，加入以下配置，同时支持 http
     @Bean
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(80);
+        tomcat.addAdditionalTomcatConnectors(connector); // 添加http
         return tomcat;
     }
 
-    private Connector createHTTPConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setPort(80);
-        connector.setRedirectPort(443);
-        return connector;
+    // 该配置激活 http的ws 和 https的wss，支持 websockt 聊天
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 
 }
